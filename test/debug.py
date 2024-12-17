52cea780d58822f8910d76037c564f55221c5136
Tomex Iskandar <34846229+tomexiskandar@users.noreply.github.com>
2024-12-17 20:35:40 +1100

    Delete test directory

--- test/debug.py
+++
@@ -1,5 +0,0 @@
-import debugpy
-debugpy.listen(5678)
-
-while True:
-    breakpoint()
--- test/test_read_msaccess.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\60145210\Documents\Database1.accdb;')
-cursor = conn.cursor()
-cursor.execute('select * from Test')
-   
-for row in cursor.fetchall():
-    print (row)
